273966810495ee645f2d991499e743972d4dc4b3
sreevinesh <sreevinesh1999@gmail.com>
2021-09-27 14:57:21 +0530

    Commit Implementation of Service and Repository GetCourses Methods

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -9,6 +9,6 @@ namespace CleanArch.Application.Interfaces
 {
     public interface ICourseService
     {
-        IEnumerable<CourseViewModel> GetCourses();
+        CourseViewModel GetCourses();
     }
 }
